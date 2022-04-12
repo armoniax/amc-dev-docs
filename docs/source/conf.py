f3f9f426fc199f2130739a517993c751ac764457
masteraychen <76104526+masteraychen@users.noreply.github.com>
2022-04-12 13:55:13 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Armonia Meta Chain'
+copyright = '2022, Armonia Blockchain Singapore'
+author = 'AMC'
 
 release = '0.1'
 version = '0.1.0'
